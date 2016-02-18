31f7b9a1352c99a1eec6661e185ca8ea1a266c46
Mrwbali <152252@vutbr.cz>
2016-02-18 12:59:19 +0100

    Firs

---
+++ Random search/Random search.cpp
@@ -0,0 +1,16 @@
+// Random search.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include "vector"
+
+
+int _tmain(int argc, _TCHAR* argv[])
+{
+	return 0;
+}
+
+double squared_x(std::vector<double>)
+{
+	for
+}
